bd7abc942558b208d7ddda48ef6f9cc5711c6301
lordjirosama <lordjiro615@gmail.com>
2026-09-13 17:36:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -102,7 +102,7 @@ class Bot(Client):
             except Exception as e:
                 self.LOGGER(__name__).warning(e)
                 self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-                self.LOGGER(__name__).info("\nBot failed to initialize correctly. Join https://t.me/UNRATED_CODER for support")
+                self.LOGGER(__name__).info("\nBot failed to initialize correctly. Join https://t.me/Anime_Hindi_Fix for support")
         else:
             self.LOGGER(__name__).warning("CHANNEL_ID is not set. Bot will not be able to store/retrieve files.")
 
@@ -149,7 +149,7 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(f"Error setting bot commands: {e}")
 
-        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @UNRATED_CODER</blockquote></b>")
+        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Anime_Hindi_Fix</blockquote></b>")
         except: pass
 
     async def stop(self, *args):
